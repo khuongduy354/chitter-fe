c6ac05e2f7c5dda77e32fb99734607f12563b9f2
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-02-17 16:35:39 +0700

    feat: theme data structure

--- src/helper/chatTheme.js
+++ src/helper/chatTheme.js
@@ -1,40 +1,50 @@
 export const defaultChatTheme = {
-  bgColor: "#282c34",
-  dividerProps: {
-    maxHeight: 300,
-    height: "20vh",
-    color: "#282c34",
+  themeName: "Untitled",
+  sender_msg_color: "#000000",
+  receiver_msg_color: "#000000",
+  bg: {
+    bgColor: "#282c34",
+    layers: [
+      {
+        translateYMin: -50,
+        translateYMax: 50,
+        margin: "-100 0 0 0",
+        imgUrl:
+          "https://oeuvmrrvzwmiyhbknqct.supabase.co/storage/v1/object/public/Emojis/Themes/DefaultTheme/layer_01_1920%20x%201080.png",
+      },
+      {
+        translateYMin: -40,
+        translateYMax: 40,
+        margin: "-400 0 0 0",
+        imgUrl:
+          "https://oeuvmrrvzwmiyhbknqct.supabase.co/storage/v1/object/public/Emojis/Themes/DefaultTheme/layer_02_1920%20x%201080.png",
+      },
+      {
+        translateYMin: -30,
+        translateYMax: 30,
+        margin: "-500 0 0 0",
+        imgUrl:
+          "https://oeuvmrrvzwmiyhbknqct.supabase.co/storage/v1/object/public/Emojis/Themes/DefaultTheme/layer_03_1920%20x%201080.png",
+      },
+      {
+        translateYMin: -20,
+        translateYMax: 20,
+        margin: "-550 0 0 0",
+        imgUrl:
+          "https://oeuvmrrvzwmiyhbknqct.supabase.co/storage/v1/object/public/Emojis/Themes/DefaultTheme/layer_04_1920%20x%201080.png",
+      },
+      {
+        translateYMin: -10,
+        translateYMax: 10,
+        margin: "-600 0 0 0",
+        imgUrl:
+          "https://oeuvmrrvzwmiyhbknqct.supabase.co/storage/v1/object/public/Emojis/Themes/DefaultTheme/layer_05_1920%20x%201080.png",
+      },
+    ],
+    divider: {
+      maxHeight: 300,
+      height: "20vh",
+      color: "#282c34",
+    },
   },
-  layers: [
-    {
-      translateY: [-50, 50],
-      margin: "-100 0 0 0",
-      imgUrl:
-        "https://oeuvmrrvzwmiyhbknqct.supabase.co/storage/v1/object/public/Emojis/Themes/DefaultTheme/layer_01_1920%20x%201080.png",
-    },
-    {
-      translateY: [-40, 40],
-      margin: "-400 0 0 0",
-      imgUrl:
-        "https://oeuvmrrvzwmiyhbknqct.supabase.co/storage/v1/object/public/Emojis/Themes/DefaultTheme/layer_02_1920%20x%201080.png",
-    },
-    {
-      translateY: [-30, 30],
-      margin: "-500 0 0 0",
-      imgUrl:
-        "https://oeuvmrrvzwmiyhbknqct.supabase.co/storage/v1/object/public/Emojis/Themes/DefaultTheme/layer_03_1920%20x%201080.png",
-    },
-    {
-      translateY: [-20, 20],
-      margin: "-550 0 0 0",
-      imgUrl:
-        "https://oeuvmrrvzwmiyhbknqct.supabase.co/storage/v1/object/public/Emojis/Themes/DefaultTheme/layer_04_1920%20x%201080.png",
-    },
-    {
-      translateY: [-10, 10],
-      margin: "-600 0 0 0",
-      imgUrl:
-        "https://oeuvmrrvzwmiyhbknqct.supabase.co/storage/v1/object/public/Emojis/Themes/DefaultTheme/layer_05_1920%20x%201080.png",
-    },
-  ],
 };

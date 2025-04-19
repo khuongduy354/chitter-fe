e47724f760dbf3ae39341316f551c09877633bf6
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2025-04-19 16:33:23 +0700

    Updates

---
+++ static/js/289.0ffe1045.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkchitter_fe=self.webpackChunkchitter_fe||[]).push([[289],{289:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.0ffe1045.chunk.js.map
